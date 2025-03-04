0ca8246ae43efe034b2d5401bffbe7958585ff18
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-04 15:48:01 +0100

    created Tablelist and table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-
-import './App.css'
+import React from 'react';
+import TableList from './components/TableList';
+import './styles/App.css';
 
 function App() {
-
+  return (
+    <div className="App">
+      <TableList />
+    </div>
+  );
 }
 
-export default App
+export default App;
