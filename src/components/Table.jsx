1e9ad1b051436ddc16df95492c4ad7afd9e8d4b7
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-04 16:02:18 +0100

    Table list where u can add tables

---
+++ src/components/Table.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Table() {
+  const handleClick = () => {};
+
+  return (
+    <div className="table" onClick={handleClick}>
+      <span>Table</span>
+    </div>
+  );
+}
+
+export default Table;
