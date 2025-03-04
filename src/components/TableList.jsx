b77f136fff3f26225738f0c82d952d10ebcf2437
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-04 16:07:28 +0100

    numbered tables

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -2,26 +2,19 @@ import React, { useState } from 'react';
 import Table from './Table';
 
 function TableList() {
-  const [tables, setTables] = useState([])
+  const [tables, setTables] = useState([]);
 
   const addTable = () => {
-
-
-    setTables([...tables, <Table key={tables.length} />]);
+    const tableNumber = tables.length + 1; 
+    setTables([...tables, <Table key={tableNumber} number={tableNumber} />]);
   };
 
-
-
-    return (
-        <div>
-            <button onClick={addTable}>Add Table</button>
-            <div>{tables}</div>
-        </div>
-    )
-
- }
+  return (
+    <div>
+      <button onClick={addTable}>Add Table</button>
+      <div>{tables}</div>
+    </div>
+  );
+}
 
 export default TableList;
-
-
-
