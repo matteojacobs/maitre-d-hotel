d940db119fe798d0b8ce9a571c1004104db340d7
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-27 12:25:11 +0100

    git revert

--- src/components/Tablelist.jsx
+++ src/components/Tablelist.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-const TableList = () => (<p>I was baaararayaya9 (Laarne)</p>);
-export default TableList;
-=======
+
 const TableList = () => <p>I was baaararayaya9 (Laarne)</p>;
 export default TableList
->>>>>>> parent of adcd7b8 (added ;)
+
