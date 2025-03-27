301100577c8d151fd477f0d54b7ab798a6050b11
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-27 12:24:00 +0100

    Revert "added ;"
    This reverts commit adcd7b8eb2a1a138f05eab4cea5878ce49261580.

--- src/components/Tablelist.jsx
+++ src/components/Tablelist.jsx
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 const TableList = () => (<p>I was baaararayaya9 (Laarne)</p>);
 export default TableList;
+=======
+const TableList = () => <p>I was baaararayaya9 (Laarne)</p>;
+export default TableList
+>>>>>>> parent of adcd7b8 (added ;)
