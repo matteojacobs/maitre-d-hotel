7718a769ed8ddf4d585ea9308206e981fe6ab8ec
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-28 10:40:51 +0100

    selectable tables

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 
-const Table = ({number}) => (
-    <li className="table">
-        <p> table: {number} </p>
-    </li>
+
+
+const TableDetail = ({ number }) => (
+  <div className="table-details">
+    <h4>Details for Table {number}</h4>
+    {/* Add more details here as needed */}
+  </div>
 );
 
 
-export const List = ({nTables}) => (
+
+const Table = ({number, onSelect}) => {
+    const handleClick = () => {
+        onSelect(number);
+
+    }
+
+    return (
+
+    <li><button onClick={handleClick} className="table">
+        <p> table: {number} </p>
+    </button></li>
+    );
+}
+
+
+export const List = ({nTables, onTableSelect}) => (
     <ul>
         {Array.from({length: nTables}).map((_, index) => (
-            <Table key={index} number={index + 1} />
+            <Table key={index} number={index + 1} onSelect={onTableSelect}/>
         ))}
         <p>amountTables = {nTables}</p>
     </ul>
@@ -22,13 +41,24 @@ export const List = ({nTables}) => (
 
 const TableList = () => {
   const [amountTables, setAmountTables] = useState(0);
+  const [selectedTable, setSelectedTable] = useState(null);
   //function to add or remove tables
   //component for a table
+
+  const handleTableSelect = (number) => {
+    {/* if number is not equal to selectedTable or null then it sets setSelectedTable to number otherwise it deselects table */}
+    setSelectedTable(number === selectedTable ? null : number)
+  }
+
   return (
     <section>
         <h3>This is the list of tables</h3>
         <button onClick={() => setAmountTables(amountTables + 1)}>Add table</button>
-        <List nTables={amountTables} />
+        <List nTables={amountTables} onTableSelect={handleTableSelect}/>
+
+        {/* only shows when a table is selected */}
+        {selectedTable && <TableDetail number={selectedTable} />}
+
     </section>
   );
 };
