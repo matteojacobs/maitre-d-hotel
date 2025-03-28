5aff789b73712987a4d052e23245f0717a4acc9d
jacobs_matteo <matteo.jacobs@student.howest.be>
2025-03-28 10:43:11 +0100

    added file for TableDetail component

---
+++ src/components/TableDetail.jsx
@@ -0,0 +1,8 @@
+const TableDetail = ({ number }) => (
+  <div className="table-details">
+    <h4>Details for Table {number}</h4>
+    {/* Add more details here as needed */}
+  </div>
+);
+
+export default TableDetail;
--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
+import TableDetail from "./TableDetail"
 
 
-
-const TableDetail = ({ number }) => (
-  <div className="table-details">
-    <h4>Details for Table {number}</h4>
-    {/* Add more details here as needed */}
-  </div>
-);
 
 
 
